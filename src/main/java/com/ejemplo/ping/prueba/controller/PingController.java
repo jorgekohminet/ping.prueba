d441e9e46d5fa45708d4bfd8011f6d280c8b6744
JorgeZavala1974 <141189831+JorgeZavala1974@users.noreply.github.com>
2026-01-03 20:32:33 -0600

    Initial commit

---
+++ src/main/java/com/ejemplo/ping/prueba/controller/PingController.java
@@ -0,0 +1,27 @@
+package com.ejemplo.ping.prueba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+
+@RestController
+@RequestMapping(path = "/start")
+public class PingController {
+
+    /**
+     * URL: http://localhost:30300/ejemploPing/start/ping
+     * @return
+     */
+    @GetMapping(path = "/ping")
+    public String pingGet(){
+        java.util.Date fechaPing = new java.util.Date();
+        String retorno = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        retorno = "Ping recibido el dia u hora: " + sdf.format(fechaPing);
+        return retorno;
+    }
+
+
+}
